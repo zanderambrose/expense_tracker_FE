611ebf528eb2667b91172c2873b5ca2ab539f0e4
Zander Ambrose <alexander.d.ambrose@gmail.com>
2022-01-06 22:44:29 -0500

    Render fetched data and add typescript interface for data schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,31 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+interface People {
+  first_name: string,
+  last_name: string,
+  age: number,
+  date_of_birth: string
+}
 
 const Home: NextPage = () => {
+  const [people, setPeople] = useState({} as People)
+
   useEffect(() => {
     async function getPeople() {
       const response = await axios.get('http://www.localhost:8000/people/')
       console.log(response)
+      setPeople(response.data.results[0])
     }
     getPeople()
   }, [])
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <ul>
+        <span>{people.first_name} {people.last_name}</span>
+      </ul>
     </>
   )
 }
