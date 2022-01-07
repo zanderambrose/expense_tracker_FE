f38513c11d1c82b3515a426f46de06794afadf3a
Zander Ambrose <alexander.d.ambrose@gmail.com>
2022-01-06 22:24:32 -0500

    Install axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
 
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    async function getPeople() {
+      fetch('http://www.localhost:8000/people/').then((res) => res.json).then((data) => console.log(data))
+    }
+    getPeople()
+  }, [])
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
