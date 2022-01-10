961f15bf142c4676c13bf85dad4baa89c5ff3fbc
Zander Ambrose <alexander.d.ambrose@gmail.com>
2022-01-09 22:10:18 -0500

    Define person type, and render mapped state

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-interface People {
+type Person = {
   first_name: string,
   last_name: string,
   age: number,
@@ -10,23 +10,30 @@ interface People {
 }
 
 const Home: NextPage = () => {
-  const [people, setPeople] = useState({} as People)
+  const [people, setPeople] = useState<Array<Person>>([])
 
   useEffect(() => {
     async function getPeople() {
       const response = await axios.get('http://www.localhost:8000/people/')
-      console.log(response)
-      setPeople(response.data.results[0])
+      console.log(response.data)
+      setPeople(response.data)
     }
     getPeople()
   }, [])
+
+  function displayUsers() {
+    return people.map((person) => {
+      return <li key={`${person.first_name} ${person.last_name}`}>{person.first_name}, {person.last_name}, was born on {person.date_of_birth} and is {person.age} years old.</li>
+    })
+  }
+
   return (
-    <>
+    <div>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
       <ul>
-        <span>{people.first_name} {people.last_name}</span>
+        {displayUsers()}
       </ul>
-    </>
+    </div>
   )
 }
 
